b273ff7577b9011047fb07d21b05b44e5dfd3ad2
trickstercode-dev <trickstercode.dev@gmail.com>
2025-05-22 20:37:48 +0700

    Make Factory and Seeder for Dummy Data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,20 +3,23 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Author;
+use App\Models\Book;
+use App\Models\Category;
+use App\Models\Rating;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-    }
+  /**
+   * Seed the application's database.
+   */
+  public function run(): void
+  {
+    Author::factory(50)->create();
+    Category::factory(100)->create();
+    Book::factory(500)->create();
+    Rating::factory(1000)->create();
+  }
 }
