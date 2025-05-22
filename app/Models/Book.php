1cb4e9a1be03587347b7a6f2425a5cb86e8f19fc
trickstercode-dev <trickstercode.dev@gmail.com>
2025-05-22 18:48:54 +0700

    Make Models and Migration

---
+++ app/Models/Book.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+  use HasFactory;
+
+  public function author()
+  {
+    return $this->belongsTo(Author::class);
+  }
+
+  public function category()
+  {
+    return $this->belongsTo(Category::class);
+  }
+
+  public function ratings()
+  {
+    return $this->hasMany(Rating::class);
+  }
+}
